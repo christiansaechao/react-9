f2d17ce84bbc1b3249780d551d592f1f0847a12f
Chris <christiansaechao23@gmail.com>
2026-07-07 18:04:48 -0700

    readd

---
+++ default-react-class/morning/week-1/src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+// import FruitList from "./mapping/FruitList";
+// import { RenderNumbers } from "./mapping/RenderNumbers";
+// import { FormatPrices } from "./mapping/FormatPrices";
+// import { EvensOnly } from "./filtering/EvensOnly"
+// import { LongWordsOnly } from "./filtering/LongWordsOnly"
+// import { MultipleConditions } from "./filtering/MultipleConditions";
+// import { SumNumbers } from "./reducing/SumNumbers";
+import { GroupUsers } from "./reducing/GroupUsers";
+
+function App() {
+  return (
+    <>
+      {/* <MultipleConditions />
+      <SumNumbers /> */}
+      <GroupUsers /> 
+    </>
+  );
+}
+
+export default App;
---
+++ default-react-class/morning/week-1/src/reducing/GroupUsers.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ProblemCard from "../extras/ProblemCard";
+
+export const GroupUsers = () => {
+  const users = [
+    { id: 1, name: "Larry", role: "admin" },
+    { id: 2, name: "Jorge", role: "user" },
+    { id: 3, name: "Karina", role: "user" },
+    { id: 4, name: "Jin", role: "admin" },
+    { id: 5, name: "Bone", role: "admin" },
+  ];
+
+  const groupedRoles = users.reduce(
+    (acc: any, curr: any) => {
+      if (curr.role === "admin") {
+        acc.admin.push(curr);
+      } else {
+        acc.user.push(curr);
+      }
+
+      return acc;
+    },
+    { admin: [], user: [] },
+  );
+
+  return (
+    <ProblemCard
+      title="Reduce 06 — Group items"
+      method="reduce"
+      question="Use reduce() to group users by role. (Example: { admin: [...], user: [...] })"
+      dataPreview={users}
+    >
+      <div>
+        {/* TODO: const grouped = users.reduce((acc, u) => {...}, {} as Record<string, typeof users>) Admin: , */}
+        {groupedRoles.admin.map(() => )}
+        {groupedRoles.user.map(() => )}
+      </div>
+    </ProblemCard>
+  );
+};
